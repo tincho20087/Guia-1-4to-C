c199444151a9bad2c514d6c6d0106a158b62585a
tincho20087 <48802391@est.ort.edu.ar>
2025-04-01 09:05:01 -0300

    Create DatosDelUsuario.cs

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int EdadUsuario;
+    public float EstaturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + "Tengo " + EdadUsuario + "Mido " + EstaturaUsuario + esDonante + " que soy donante de organos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
