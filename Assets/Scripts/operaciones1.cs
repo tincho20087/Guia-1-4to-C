e6a956664508ceeec5b119c826c206618805b35a
tincho20087 <48802391@est.ort.edu.ar>
2025-04-03 12:07:02 -0300

    Create operaciones1.cs

---
+++ Assets/Scripts/operaciones1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class operaciones1 : MonoBehaviour
+{
+    int num1 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = num1 + 2;
+        Debug.Log(num1);
+        num1 = num1 * num1;
+        Debug.Log(num1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
