f8eda2a676521f62ff05f472dccee213f8d50dd6
tincho20087 <48802391@est.ort.edu.ar>
2025-04-03 11:18:04 -0300

    Update DatosDelUsuario.cs

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,7 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario + "Tengo " + EdadUsuario + "Mido " + EstaturaUsuario + esDonante + " que soy donante de organos");
+        Debug.Log("Mi nombre es " + nombreUsuario + " Tengo " + EdadUsuario + " Mido " + EstaturaUsuario + " es " +  esDonante  + " que soy donante de organos");
     }
 
     // Update is called once per frame
