b6c9506a9f81d0dd0b7075088513a9dcbd6128b8
tincho20087 <48802391@est.ort.edu.ar>
2025-04-08 09:01:27 -0300

    Create NewBehaviourScript.cs

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float montoAconvertir;
+    public string codmoneda;
+
+    float montoconv;
+    float cotizacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (montoAconvertir < 1000)
+        {
+            Debug.Log("El monto minimo es 1000");
+            return;
+        }
+
+        if (codmoneda == "D")
+        {
+            cotizacion = 1134.5f;
+        }
+
+        else if (codmoneda == "R")
+        {
+            cotizacion = 211.1f;
+        }
+
+        else if (codmoneda == "E")
+        {
+            cotizacion = 1397.16f;
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+
+
+        montoAconvertir = montoAconvertir / cotizacion;
+
+        Debug.Log("Monto convertido " + montoAconvertir);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
